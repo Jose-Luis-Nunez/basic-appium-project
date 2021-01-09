c6a18a05d8bd82c97af5ea278e05cf61f66efd47
Jose-Luis-Nunez <josenunez@outlook.de>
2021-01-09 01:30:00 +0100

    add dependencies and README file

---
+++ README.md
@@ -0,0 +1,66 @@
+# Basic Appium Project
+
+This **[Kotlin](https://kotlinlang.org)** project is an example of the implementation of an [Appium](http://appium.io/) test project with [FluentLenium](https://fluentlenium.com) and [Gradle](https://gradle.org)
+
+To run the tests locally:
+1. start emulator
+2. start appium server
+3. run test or execute command in terminal `./gradlew clean test`
+> the test fails if the emulator or the appium server is not running
+## Infos
+### Appium
+#### Install appium
+`npm install -g appium`
+#### Start appium server
+`appium`
+
+### Emulator
+#### Find installed emulators
+`emulator -list-avds`
+#### Start specific emulator
+`emulator -avd [emulator name]`
+
+## Write tests
+#### Create Page Object 
+The Page Object Pattern can be used to specify elements etc.
+- Every page object class inherits from FluentPage()
+- use @AndroidFindBy and selector to identify elements
+
+``` java
+class StartPage : FluentPage() {
+    @AndroidFindBy(id = "tv_beer")
+    private lateinit var itemName: FluentWebElement
+    
+    .
+    .
+    .
+}
+```
+
+To instantiate a page object in a test class just use @Page:
+```
+@Page
+lateinit var startPage: StartPage
+```
+#### Test example 
+Every test class inherits from AppiumTestSetup()
+``` java
+class StartPageTest : AppiumTestSetup() {
+
+    @Page
+    lateinit var startPage: StartPage
+
+    @Test
+    fun checkPriceForSpecificAmountOfBeers() {
+        val expectedAmount = 5
+        val expectedTotalPrice = 995
+
+        with(startPage) {
+            insertNewPrice("1.99")
+            addBeers(5)
+        }
+        assertThat(startPage.getAmount()).isEqualTo(expectedAmount)
+        assertThat(startPage.getTotalPrice()).isEqualTo(expectedTotalPrice)
+    }
+}
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,79 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20-M1"
+    kotlin("jvm") version "1.3.70"
 }
 
-group = "me.user"
+group = "me.josenunez"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
-    }
+    jcenter()
 }
 
 dependencies {
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    val fluentleniumVersion = "4.3.1"
+    val seleniumVersion = "3.141.59"
+    val assertjVersion = "3.6.1"
+    val assertKVersion = "0.22"
+    val appiumVersion = "7.0.0"
+    val jUnitVersion = "5.5.2"
+
+    testImplementation(
+        group = "org.junit.jupiter",
+        name = "junit-jupiter",
+        version = jUnitVersion
+    )
+    implementation(
+        group = "com.willowtreeapps.assertk",
+        name = "assertk",
+        version = assertKVersion
+    )
+    testImplementation(
+        group = "org.assertj",
+        name = "assertj-core",
+        version = assertjVersion
+    )
+    implementation(
+        group = "io.appium",
+        name = "java-client",
+        version = appiumVersion
+    )
+    implementation(
+        group = "org.seleniumhq.selenium",
+        name = "selenium-java",
+        version = seleniumVersion
+    )
+    testImplementation(
+        group = "org.fluentlenium",
+        name = "fluentlenium-junit-jupiter",
+        version = fluentleniumVersion
+    )
+    implementation(
+        group = "org.fluentlenium",
+        name = "fluentlenium-core",
+        version = fluentleniumVersion
+    )
+    implementation(
+        group = "org.fluentlenium",
+        name = "fluentlenium-assertj",
+        version = fluentleniumVersion
+    )
+    testImplementation(
+        group = "io.strikt",
+        name = "strikt-core",
+        version = "0.27.0"
+    )
+    implementation(kotlin("stdlib-jdk8"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+    }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
