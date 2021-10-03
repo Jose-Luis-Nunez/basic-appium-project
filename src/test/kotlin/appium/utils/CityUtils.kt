23cd86213b39bb59496b25bd8ac594cc25babaed
Jose-Luis-Nunez <josenunez@outlook.de>
2021-10-04 00:08:26 +0200

    add hamburg city

--- src/test/kotlin/appium/utils/CityUtils.kt
+++ src/test/kotlin/appium/utils/CityUtils.kt
@@ -9,6 +9,14 @@ class CityUtils {
                     altitude = 20.0
             )
         }
+
+        fun getHamburgLocation(): City {
+            return City(
+                    latitude = 53.5523841,
+                    longitude = 10.0060635,
+                    altitude = 17.67
+            )
+        }
     }
 
     data class City(val latitude: Double, val longitude: Double, val altitude: Double)
