206feb5b2713267d648b36a3f30b5da870069e48
Jose-Luis-Nunez <josenunez@outlook.de>
2021-10-04 00:00:03 +0200

    add test

--- src/test/kotlin/appium/test/BeermatTest.kt
+++ src/test/kotlin/appium/test/BeermatTest.kt
@@ -90,7 +90,9 @@ class BeermatTest : AppiumTestSetup() {
         }
 
         assertThat(beermat.getDrinkName()).contains("Cola")
-    }   @Test
+    }
+
+    @Test
     fun checkTextField3() {
         beermat {
             enterDrinkName("Cola")
