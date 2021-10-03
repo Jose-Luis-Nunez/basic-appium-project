b458dd719e3d85b8fd9986b8e0455ab4bfe2b4bd
Jose-Luis-Nunez <josenunez@outlook.de>
2021-10-03 07:58:53 +0200

    update android capabilities

--- src/main/kotlin/appium/device/Android.kt
+++ src/main/kotlin/appium/device/Android.kt
@@ -1,9 +1,11 @@
 package appium.device
 
 import appium.utils.PropertiesReader
-import io.appium.java_client.remote.MobileCapabilityType.APP
 import io.appium.java_client.remote.MobileCapabilityType.AUTOMATION_NAME
 import io.appium.java_client.remote.MobileCapabilityType.DEVICE_NAME
+import io.appium.java_client.remote.MobileCapabilityType.APP
+import io.appium.java_client.remote.MobileCapabilityType.FULL_RESET
+
 import org.openqa.selenium.Capabilities
 import org.openqa.selenium.remote.CapabilityType.PLATFORM_NAME
 import org.openqa.selenium.remote.DesiredCapabilities
@@ -14,9 +16,15 @@ class Android : Platform {
     override fun getCapabilities(): Capabilities? {
         return DesiredCapabilities().apply {
             setCapability("autoGrantPermissions", true)
+            setCapability("gpsEnabled", true)
+            setCapability("locale", "DE")
+            setCapability("language", "de")
+            setCapability("newCommandTimeout", 120)
+            setCapability("disableWindowAnimation", true)
             setCapability(PLATFORM_NAME, "Android")
             setCapability(AUTOMATION_NAME, "UiAutomator2")
             setCapability(DEVICE_NAME, "Android Emulator")
+            setCapability(FULL_RESET, true)
             setCapability(APP, getTestAppPath())
         }
     }
